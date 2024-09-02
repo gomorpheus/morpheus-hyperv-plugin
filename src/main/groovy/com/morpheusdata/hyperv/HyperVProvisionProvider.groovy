f37253d1a9b1c460c12a8823ca8a68d3c7b4b484
rahulray19 <100296364+rahulray19@users.noreply.github.com>
2024-09-02 13:45:31 +0530

    hyper-v plugin: start instance

--- src/main/groovy/com/morpheusdata/hyperv/HyperVProvisionProvider.groovy
+++ src/main/groovy/com/morpheusdata/hyperv/HyperVProvisionProvider.groovy
@@ -249,7 +249,23 @@ class HyperVProvisionProvider extends AbstractProvisionProvider implements Workl
 	 */
 	@Override
 	ServiceResponse startWorkload(Workload workload) {
-		return ServiceResponse.success()
+		def rtn = ServiceResponse.prepare()
+		try {
+			if (workload.server?.externalId) {
+				def hypervOpts = HypervOptsUtility.getAllHypervWorloadOpts(workload)
+				def results = apiService.startServer(hypervOpts, hypervOpts.name)
+				if (results.success == true) {
+					rtn.success = true
+				}
+			} else {
+				rtn.success = false
+				rtn.msg = 'externalId not found'
+			}
+		} catch (e) {
+			log.error("startWorkload error: ${e}", e)
+			rtn.msg = e.message
+		}
+		return rtn
 	}
 
 	/**
